6f5fbc84d1f7a5df94416f6d217dd529393dcfd1
Kiyoka-wy <757008272@qq.com>
2018-12-12 22:11:33 +0800

    添加详情（补充）

--- pages/task/published/published.js
+++ pages/task/published/published.js
@@ -1,21 +1,43 @@
-// pages/task/published/published.js
 var app = getApp();
 
 Page({
   data: {
-    current: 'tab1',
-    current_scroll: 'tab1'
+    currentData: 0,
+    tasks: [
+      {
+        taskID: "001",
+        title: "我是任务的标题1",
+        fromLocation: "我是任务的交付地点",
+        Timeddl: "15：00"
+      },
+      {
+        taskID: "002",
+        title: "我是任务的标题2",
+        fromLocation: "我是任务的交付地点2",
+        Timeddl: "16：00"
+      }
+    ]
+
   },
 
-  handleChange({ detail }) {
-    this.setData({
-      current: detail.key
-    });
+  //获取当前滑块的index
+  bindchange: function (e) {
+    const that = this;
+    that.setData({
+      currentData: e.detail.current
+    })
   },
+  //点击切换，滑块index赋值
+  checkCurrent: function (e) {
+    const that = this;
 
-  handleChangeScroll({ detail }) {
-    this.setData({
-      current_scroll: detail.key
-    });
+    if (that.data.currentData === e.target.dataset.current) {
+      return false;
+    } else {
+
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
   }
 })
