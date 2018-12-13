a556563ae1cf740ead19242e72850b1b66359e3b
Kiyoka-wy <757008272@qq.com>
2018-12-13 23:21:48 +0800

    任务执行期限倒计时

--- pages/task/detail_acc/detail.js
+++ pages/task/detail_acc/detail.js
@@ -7,13 +7,14 @@ Page({
     fromLocation: "任务执行地点",
     toLocation: "任务交付的地点",
     HideContent: "任务隐藏的字段啊",
+    limit:3,
     targetTime: 0,
     clearTimer: false
   },
   
   onLoad: function (options) {
     this.setData({
-      targetTime: new Date().getTime() + 6430000,
+      targetTime: new Date().getTime() + 1000*60*60*this.data.limit,
       title: options.title
     })
   },
